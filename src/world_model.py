64ebc7d182a30675bd93d41b85f340e738b994f8
Kian Behzad <kian.behzad@gmail.com>
2021-04-22 15:31:13 +0430

    [worldmodel] implement the stick action class for worldmodel class

--- src/world_model.py
+++ src/world_model.py
@@ -1,4 +1,5 @@
 from geo import *
+from kaggle_environments.envs.football.helpers import *
 
 class Ball:
     def __init__(self):
@@ -14,3 +15,31 @@ class Agent:
         self.tiredness = 0.0
         self.has_yellowcard = False
         self.id = -1
+
+
+class StickyActions:
+    def __init__(self):
+        self.top = False
+        self.bottom = False
+        self.left = False
+        self.right = False
+
+        self.top_left = False
+        self.top_right = False
+        self.bottom_left = False
+        self.bottom_right = False
+
+        self.sprint = False
+        self.dribble = False
+
+    def update(self, obs):
+        self.top = Action.Top in obs['sticky_actions']
+        self.bottom = Action.Bottom in obs['sticky_actions']
+        self.left = Action.Left in obs['sticky_actions']
+        self.right = Action.Right in obs['sticky_actions']
+        self.top_left = Action.TopLeft in obs['sticky_actions']
+        self.top_right = Action.TopRight in obs['sticky_actions']
+        self.bottom_left = Action.BottomLeft in obs['sticky_actions']
+        self.bottom_right = Action.BottomRight in obs['sticky_actions']
+        self.sprint = Action.Sprint in obs['sticky_actions']
+        self.dribble = Action.Dribble in obs['sticky_actions']
